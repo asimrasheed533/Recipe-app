ea4aeb77c3c093698656105a3ea2317a904dafa1
Asim Rasheed <asimrasheed442@gmail.com>
2024-05-04 06:03:43 +0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const monogoose = require("mongoose");
 const cors = require("cors");
+const userRouter = require("./router/userRouter");
+const category = require("./router/categories");
+const product = require("./router/Products");
 const dotenv = require("dotenv");
-const userRouter = require("./router/userRouter")
-const category = require("./router/categories")
 dotenv.config();
 
 // api config
 const app = express();
-const port = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
 app.use(cors());
 // db config
-monogoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("Database connected")
-})
+monogoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Database connected");
+});
 // api endpoints
-app.get("/", (req, res)=>{
-    res.status(200).json({
-        message:"welcome to the api"
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "welcome to the api",
+  });
 });
 app.use("/api/users", userRouter);
-app.use("/api/categories", category);  
+app.use("/api/categories", category);
+app.use("/api/products", product);
 
 // listener
-app.listen(port, ()=>
-console.log(`Server is running on port http://localhost:${port}/`)
-) 
+app.listen(port, () =>
+  console.log(`Server is running on port http://localhost:${port}/`)
+);
