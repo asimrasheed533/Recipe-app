df0977ff82943a9231ba92b993a48b7831166008
Asim Rasheed <asimrasheed442@gmail.com>
2024-05-04 06:03:45 +0500

    Update Products.js

--- server/src/router/Products.js
+++ server/src/router/Products.js
@@ -1,37 +1,56 @@
-const express = require ("express");
-const mongoose = require ("mongoose");
-
-const router = express.Router()
+const express = require("express");
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: String,
-    brand: String,
-    price: Number,
-    img: String,
-    stock: Number,
-    category: String,
-    description: String,
-    isFeatured: Boolean,
-    isActive: Boolean,
-})
+  name: String,
+  brand: String,
+  price: Number,
+  img: String,
+  stock: Number,
+  category: String,
+  description: String,
+  isFeatured: Boolean,
+  isActive: Boolean,
+});
 
-const Product = mongoose.model("Products", schema)
+const Product = mongoose.model("Products", schema);
+const router = express.Router();
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-router.get("/", async(req, res)=>{
-    try{
-        const products = await Product.find()
-        res.send(products)
-    } catch (err){
-        console.log(err)
-    }
-})
+router.get("/:id", async (req, res) => {
+  try {
+    const products = await Product.findById();
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-router.get("/:id", async(req, res)=>{
-    try{
-        const products = await Product.findById()
-        res.Send(products)
-    } catch(err){
-        console.log(err)
-    }
-})
+router.post("/add", async (req, res) => {
+  try {
+    const product = new Product({
+      name: req.body.name,
+      brand: req.body.brand,
+      time: req.body.time,
+      img: req.body.img,
+      stock: req.body.stock,
+      category: req.body.category,
+      description: req.body.description,
+      isFeatured: req.body.isFeatured,
+      isActive: req.body.isActive,
+    });
+    await product.save();
+    res.send(product);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+module.exports = router;
