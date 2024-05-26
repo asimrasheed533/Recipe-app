cf8461a04d110284c8423923451b24ae055390ef
Asim Rasheed <asimrasheed442@gmail.com>
2024-05-26 17:12:37 +0500

    updated packege

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,7 +1,8 @@
 import axiosBase from "axios";
 
 const axios = axiosBase.create({
-    baseURL: "http://192.168.100.198:9000/api",
+  // baseURL: "http://192.168.100.198:9000/api",
+  baseURL: "http://192.168.0.111:9000/api",
 });
 
 export default axios;
